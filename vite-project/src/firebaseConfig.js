ad8a25ba40caca33763151a02e08b9fadce1e0a7
Nabil <nabilpasha230606@gmail.com>
2024-11-01 23:30:28 +0700

    Added Firebase as the primary database

--- vite-project/src/firebaseConfig.js
+++ vite-project/src/firebaseConfig.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZhhMAFA83Jae0pe3CLYJRuP3SNkEPoAg",
-  authDomain: "barkas-layo-64bac.firebaseapp.com",
-  projectId: "barkas-layo-64bac",
-  storageBucket: "barkas-layo-64bac.firebasestorage.app",
-  messagingSenderId: "217595564781",
-  appId: "1:217595564781:web:d57cea182cb92073fa32e4"
+    apiKey: "AIzaSyCZhhMAFA83Jae0pe3CLYJRuP3SNkEPoAg",
+    authDomain: "barkas-layo-64bac.firebaseapp.com",
+    projectId: "barkas-layo-64bac",
+    storageBucket: "barkas-layo-64bac.firebasestorage.app",
+    messagingSenderId: "217595564781",
+    appId: "1:217595564781:web:d57cea182cb92073fa32e4",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db };
+export { auth };
