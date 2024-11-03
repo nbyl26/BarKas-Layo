8fb927b5552e55412046d3e985858373d634915c
Nabil <nabilpasha230606@gmail.com>
2024-11-04 01:07:07 +0700

    Complete data retrieval in database for user profile

--- vite-project/src/auth/authService.js
+++ vite-project/src/auth/authService.js
@@ -4,7 +4,6 @@ import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "fire
 import { db } from '../firebaseConfig';
 import { doc, setDoc } from "firebase/firestore";
 
-// Fungsi untuk registrasi pengguna
 export const registerUser = async (email, password, name) => {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
@@ -16,22 +15,17 @@ export const registerUser = async (email, password, name) => {
       registeredDate: Date.now() 
   });
 
-    // Simpan status login di cookie
     Cookies.set('user', JSON.stringify(user), { expires: 1 }); // Cookie akan kedaluwarsa dalam 1 hari
     return user;
 };
 
-// Fungsi untuk login pengguna
 export const loginUser = async (email, password) => {
     const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    // Simpan status login di cookie
     Cookies.set('user', JSON.stringify(userCredential.user), { expires: 1 }); // Cookie akan kedaluwarsa dalam 1 hari
     return userCredential.user;
 };
 
-// Fungsi untuk logout pengguna
 export const logoutUser = async () => {
     await auth.signOut();
-    // Hapus cookie saat logout
     Cookies.remove('user');
 };
