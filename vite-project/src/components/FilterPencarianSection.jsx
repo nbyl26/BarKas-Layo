5cd880dbf2cdcb6c62f9e3361534c01511b4d9d2
Nabil <nabilpasha230606@gmail.com>
2024-11-07 01:18:04 +0700

    Fix Code and Display /Filter

--- vite-project/src/components/FilterPencarianSection.jsx
+++ vite-project/src/components/FilterPencarianSection.jsx
@@ -31,7 +31,11 @@ function FilterPencarianSection() {
       try {
         // Mengambil data produk yang sesuai dengan query
         const querySnapshot = await getDocs(q);
+        console.log('Query Snapshot:', querySnapshot); // Debugging query snapshot
+
         const filteredProducts = querySnapshot.docs.map(doc => doc.data());
+        console.log('Filtered Products:', filteredProducts); // Debugging filtered products
+
         setProducts(filteredProducts); // Menyimpan produk yang sudah difilter
       } catch (error) {
         console.error('Error fetching filtered products:', error);
@@ -43,21 +47,27 @@ function FilterPencarianSection() {
 
   return (
     <section className="filtered-products">
-      <h2>Produk yang Terfilter</h2>
+        <div className="container">
+      <h1><span>Produk yang</span> Terfilter</h1>
       <div className="products-list">
         {products.length > 0 ? (
           products.map((product, index) => (
             <div key={index} className="product-item">
-              <img src={product.image} alt={product.name} />
+              <img src={product.image} alt={product.name} className="item-image" />
+              <div className="item-info">
               <h3>{product.name}</h3>
               <p>{product.description}</p>
-              <p>{product.price}</p>
               <p>{product.condition}</p>
+              <p className="price">{product.price}</p>
+              <a href={`/DetailBarang?item=${product.id}`} className="detail-button">Detail</a>
+              </div>
             </div>
+            
           ))
         ) : (
           <p>Produk tidak ditemukan dengan filter yang dipilih.</p>
         )}
+      </div>
       </div>
     </section>
   );
