5f88b49e229889296c970c8f646108c8060c7522
Nabil <nabilpasha230606@gmail.com>
2024-11-11 23:04:59 +0700

    Added AuthContext to handle logged in users

---
+++ vite-project/src/components/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebaseConfig';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, setCurrentUser);
+        return () => unsubscribe(); // Cleanup when component is unmounted
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ currentUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
