8206239e7471db00491baf46e1187dbc4fec55cb
Nabil <nabilpasha230606@gmail.com>
2024-11-12 22:12:49 +0700

    Improve the appearance of the AllItemsPages

--- vite-project/src/pages/SemuaBarang.jsx
+++ vite-project/src/pages/SemuaBarang.jsx
@@ -1,14 +1,18 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import SemuaBarangSection from "../components/SemuaBarangSection"
+import '../assets/styles/SemuaBarang.css' 
 
 function SemuaBarang() {
   return (
-    <>
-    <Header />
-    <SemuaBarangSection />
-    <Footer />
-    </>
+
+    <div className="all-pages-container">
+      <div className="main-content">
+        <Header />
+        <SemuaBarangSection />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
