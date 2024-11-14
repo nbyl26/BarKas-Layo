4283d24721b4c7dba1d39dedd3283e7e4d3e0f48
Nabil <nabilpasha230606@gmail.com>
2024-11-14 23:31:50 +0700

    Improve the code structure on the Home Page

--- vite-project/src/pages/Beranda.jsx
+++ vite-project/src/pages/Beranda.jsx
@@ -1,23 +1,25 @@
 import Header from "../components/Header"
-import Footer from "../components/Footer" 
+import Footer from "../components/Footer"
 import FilterPencarian from "../components/FilterPencarian"
 import Hero from "../components/Hero"
 import KategoriPopuler from "../components/KategoriPopuler"
 import BarangTerbaru from "../components/BarangTerbaru"
 import ChatBot from "../components/ChatBot"
+import '../assets/styles/Beranda.css'
 
 function Beranda() {
   return (
-    <>
-    <Header />
-    <FilterPencarian />
-    <Hero />
-    <KategoriPopuler />
-    <BarangTerbaru />
-    <Footer />
-    <ChatBot />
-    
-    </>
+    <div className="beranda-pages-container">
+      <div className="main-content">
+        <Header />
+        <FilterPencarian />
+        <Hero />
+        <KategoriPopuler />
+        <BarangTerbaru />
+      </div>
+      <Footer />
+      <ChatBot />
+    </div>
   )
 }
 
