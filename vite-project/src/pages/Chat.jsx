accc8913dba3c9d6540227b1886b641d69032210
Nabil <nabilpasha230606@gmail.com>
2024-11-14 23:34:12 +0700

    Improve the code structure on the Chat Page

--- vite-project/src/pages/Chat.jsx
+++ vite-project/src/pages/Chat.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Header2 from '../components/Header2'
 import ChatSection from '../components/chatSection'
 import Footer from '../components/Footer'
+import '../assets/styles/Chat.css'
 
 function Chat() {
   return (
-    <>
+    <div className="chat-pages-container">
+      <div className="main-content">
         <Header2 />
         <ChatSection />
-        <Footer />  
-    </>
+      </div>
+      <Footer />
+    </div>
   )
 }
 
