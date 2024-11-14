04cc315c370b6b2182aed6b0c967d5a302d59ab7
Nabil <nabilpasha230606@gmail.com>
2024-11-14 23:37:24 +0700

    Improve the code structure on the Filter Items Page

--- vite-project/src/pages/FilterPencarianPages.jsx
+++ vite-project/src/pages/FilterPencarianPages.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from '../components/Header';
 import FilterPencarianSection from '../components/FilterPencarianSection';
 import Footer from '../components/Footer';
+import '../assets/styles/FilterPencarianPages.css';
 
 function FilterPencarianPages() {
   return (
-    <div>
-      <Header />
-      <FilterPencarianSection />
+    <div className="filter-pages-container">
+      <div className="main-content">
+        <Header />
+        <FilterPencarianSection />
+      </div>
       <Footer />
     </div>
   );
