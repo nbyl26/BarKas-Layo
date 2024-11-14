b07656ddb5948569e8174bd15e021684877b9a75
Nabil <nabilpasha230606@gmail.com>
2024-11-14 23:28:42 +0700

    Improve the code structure on the AccountProfile Page

--- vite-project/src/pages/AccountProfile.jsx
+++ vite-project/src/pages/AccountProfile.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import AccountProfileSection from '../components/AccountProfileSection';
 import Header2 from '../components/Header2';
-import Footer from '../components/Footer';  
+import Footer from '../components/Footer';
+import '../assets/styles/AccountProfile.css';
+
 function AccountProfile() {
     return (
-        <>
-        <Header2 />
-        <AccountProfileSection />
-        <Footer />
-        </>
+        <div className="profile-pages-container">
+            <Header2 />
+            <div className="main-content">
+                <AccountProfileSection />
+            </div>
+            <Footer />
+        </div>
     );
 }
 
