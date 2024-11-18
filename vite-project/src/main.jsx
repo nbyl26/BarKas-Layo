c551bf4f0d27560aad5997098d0757a9bd628257
Nabil <nabilpasha230606@gmail.com>
2024-11-18 22:55:05 +0700

    Improve the appearance of the SearchPages

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { AuthProvider } from './AuthProvider.jsx'; // pastikan AuthProvider sudah diimport dengan benar
+import { AuthProvider } from './AuthProvider.jsx'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
